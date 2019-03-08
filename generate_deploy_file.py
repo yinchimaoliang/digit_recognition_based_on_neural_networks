c6a0d8094f9009e017820c04c6751e3454042030
yinchimaoliang <liyinhao0413@qq.com>
2019-03-08 09:05:44 +0800

    Changed the structure of the neural network

--- generate_deploy_file.py
+++ generate_deploy_file.py
@@ -4,21 +4,21 @@ deploy=root+'mnist/deploy.prototxt'    #文件保存路径
 
 def create_deploy():
     #少了第一层，data层
-    conv1 = L.Convolution(bottom = "data", kernel_size=5, stride=1, num_output=20, pad=0, weight_filler=dict(type='xavier'))
+    conv1 = L.Convolution(bottom = "data", kernel_size=5, stride=1, num_output=20, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu1 = L.ReLU(conv1, in_place=True)
     # 卷积层
-    conv2 = L.Convolution(relu1, kernel_size=5, stride=1, num_output=50, pad=0, weight_filler=dict(type='xavier'))
+    conv2 = L.Convolution(relu1, kernel_size=5, stride=1, num_output=50, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu2 = L.ReLU(conv2, in_place=True)
     # 池化层
     pool1 = L.Pooling(relu2, pool=P.Pooling.MAX, kernel_size=2, stride=2)
     # 第二层：卷积层
-    conv3 = L.Convolution(pool1, kernel_size=5, stride=1, num_output=20, pad=0, weight_filler=dict(type='xavier'))
+    conv3 = L.Convolution(pool1, kernel_size=5, stride=1, num_output=20, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu3 = L.ReLU(conv3, in_place=True)
     # 卷积层
-    conv4 = L.Convolution(relu3, kernel_size=5, stride=1, num_output=50, pad=0, weight_filler=dict(type='xavier'))
+    conv4 = L.Convolution(relu3, kernel_size=5, stride=1, num_output=50, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu4 = L.ReLU(conv4, in_place=True)
     # 全连接层
