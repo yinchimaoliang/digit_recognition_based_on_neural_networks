ac2ee7cf9b31dd930cad46ea71f07c8e331b13ab
yinchimaoliang <liyinhao0413@qq.com>
2019-03-08 09:04:37 +0800

    Update generate_training_file.py

--- generate_training_file.py
+++ generate_training_file.py
@@ -13,21 +13,21 @@ def Lenet(img_list, batch_size, include_acc=False):
     data, label = L.ImageData(source=img_list, batch_size=batch_size, ntop=2, root_folder=root,
                               transform_param=dict(scale=0.00390625))#将输入数据1/255归一到0-255
     # 第二层：卷积层
-    conv1 = L.Convolution(data, kernel_size=5, stride=1, num_output=20, pad=0, weight_filler=dict(type='xavier'))
+    conv1 = L.Convolution(data, kernel_size=5, stride=1, num_output=20, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu1 = L.ReLU(conv1, in_place=True)
     # 卷积层
-    conv2 = L.Convolution(relu1, kernel_size=5, stride=1, num_output=50, pad=0, weight_filler=dict(type='xavier'))
+    conv2 = L.Convolution(relu1, kernel_size=5, stride=1, num_output=50, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu2 = L.ReLU(conv2, in_place=True)
     # 池化层
     pool1 = L.Pooling(relu2, pool=P.Pooling.MAX, kernel_size=2, stride=2)
     # 第二层：卷积层
-    conv3 = L.Convolution(pool1, kernel_size=5, stride=1, num_output=20, pad=0, weight_filler=dict(type='xavier'))
+    conv3 = L.Convolution(pool1, kernel_size=5, stride=1, num_output=20, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu3 = L.ReLU(conv3, in_place=True)
     # 卷积层
-    conv4 = L.Convolution(relu3, kernel_size=5, stride=1, num_output=50, pad=0, weight_filler=dict(type='xavier'))
+    conv4 = L.Convolution(relu3, kernel_size=5, stride=1, num_output=50, pad=4, weight_filler=dict(type='xavier'))
     # 激活函数层
     relu4 = L.ReLU(conv4, in_place=True)
     # 全连接层
